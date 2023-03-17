13386b98082f810f9e445f4877e14fe5fdc1c181
juna3066 <juna3066@qq.com>
2023-03-18 02:05:17 +0800

    stream 中间操作

--- src/main/java/org/jun/practice/stream/StreamDemo.java
+++ src/main/java/org/jun/practice/stream/StreamDemo.java
@@ -5,11 +5,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
 /**
  * 流(Stream)知识点：
@@ -19,7 +17,7 @@ import java.util.stream.Stream;
  * 注意点：
  * 1.流自己不存储元素
  * 2.流不改变源对象,返回新的Stream
- * 3.流操作是延迟的,会等到需要结果的时候才执行。
+ * 3.流操作是延迟的,会等到需要结果的时候才执行。(惰性求值,遇到终止操作，所有操作才一次全部执行)
  *
  * 三步骤：
  *      创建操作：
@@ -34,13 +32,12 @@ public class StreamDemo {
 
     List<Emp> empList = Arrays.asList(
             new Emp(100L,"李一",27,24000),
-            new Emp(100L,"刘二",23,15000),
-            new Emp(100L,"张三",24,13000),
-            new Emp(100L,"李四",26,30000),
-            new Emp(100L,"王五",30,35000),
-            new Emp(100L,"小刘",35,40000),
-            new Emp(100L,"田七",25,21500)
-
+            new Emp(101L,"刘二",23,15000),
+            new Emp(102L,"张三",24,13000),
+            new Emp(103L,"李四",26,30000),
+            new Emp(103L,"李四",26,30000),
+            new Emp(104L,"王五",25,35000),
+            new Emp(105L,"小刘",22,40000)
     );
 
     /**
@@ -77,5 +74,128 @@ public class StreamDemo {
         Stream<Double> generate = Stream.generate(Math::random).limit(6);
         generate.forEach(System.out::println);
     }
+
+    /**
+     * 中间操作
+     *      1.筛选与切片（筛选 去重 截断 跳过）
+     *          Stream的filter接口
+     *          Stream的distinct接口（通过所生成元素的hashCode和equals方法去除重复元素）
+     *          Stream的limit接口
+     *          Stream的skip接口
+     *
+     *      2.映射
+     *          map
+     *          mapToInt/Long/Double
+     *          flatMap
+     *
+     *      3.排序
+     *          自然排序 生成新流
+     *          按比较器排序 生成新流
+     */
+    @Test
+    public void demo2() {
+        //middleOpt01();
+        //middleOpt02();
+        middleOpt03();
+
+    }
+
+    /**
+     * 中间操作03 sort
+     */
+    private void middleOpt03() {
+        //sort() 自然排序 生成新流
+        empList.stream().map(Emp::getName).limit(3).sorted().forEach(System.out::println);
+        System.out.println();
+        empList.stream().map(Emp::getId).limit(3).sorted().forEach(System.out::println);
+        System.out.println();
+
+        //按比较器排序 生成新流
+        empList.stream().sorted(
+                (e1, e2) -> (int) (e2.getSalary() - e1.getSalary())
+        ).forEach(System.out::println);
+    }
+
+    /**
+     * 中间操作02 map
+     */
+    private void middleOpt02() {
+        Stream<Emp> empStream = empList.stream();
+        Stream<String> stringStream = empStream.map(Emp::getName).distinct();
+        stringStream.forEach(System.out::println);
+        System.out.println();
+
+        List<String> list = Arrays.asList("aa", "bb", "cc");
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+        System.out.println();
+
+        Stream<String> stream = list.stream();
+        //stream.flatMap(s -> Stream.of(s,1)).forEach(System.out::println);
+        Stream<Character> characterStream = stream.flatMap(StreamDemo::getCharacterStream);
+        characterStream.forEach(System.out::println);
+        System.out.println();
+
+        Stream<Stream<Character>> streamStream = list.stream().map(StreamDemo::getCharacterStream);
+        streamStream.forEach(item->{
+            item.forEach(System.out::println);
+        });
+        System.out.println();
+
+        // Int/Long/DoubleStream
+        DoubleStream doubleStream = empList.stream().mapToDouble(Emp::getSalary);
+        doubleStream.forEach(System.out::println);
+    }
+
+    private static Stream<Character> getCharacterStream(String s) {
+        List<Character> characterList = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            characterList.add(ch);
+        }
+        return characterList.stream();
+    }
+
+
+    /**
+     * 筛选与切片
+     */
+    private void middleOpt01() {
+        //demoFilter();
+
+        //todo 关于移除Emp @EqualsAndHashCode后 此处仍能成功过滤 李一的原因 类的成员？把李一当成一个对象引用了？
+        empList.stream().distinct().forEach(System.out::println);
+        System.out.println();
+
+        //limit skip 效果互补
+        empList.stream().filter(emp->{
+            //test打印几次？ 3
+            System.out.println("test");
+            return emp.getAge()< 25;
+        }).limit(2).forEach(System.out::println);
+        System.out.println();
+
+        empList.stream().filter(emp -> emp.getAge() < 25).skip(2).forEach(System.out::println);
+    }
+
+    private void demoFilter() {
+        Stream<Emp> empStream = empList.stream();
+        Stream<Emp> empStream1 = empStream.filter(emp -> {
+            //惰性求值
+            System.out.println("中间操作");
+            //return emp.getAge() >=25 && emp.getAge() <= 30;
+            return emp.getAge() < 25 ;
+        });
+
+        //内部迭代：StreamAPI 内部完成迭代
+        empStream1.forEach(System.out::println);
+        //empStream empStream1 都关闭了  执行结束操作,相关的流均会关闭，无法再此使用
+
+        //外部迭代
+        Iterator<Emp> iterator = empList.iterator();
+        while (iterator.hasNext()){
+            System.out.println("iterator.next() = " + iterator.next());
+        }
+    }
+
 
 }
